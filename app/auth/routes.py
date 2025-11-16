dfc11a5d25ac5ce449cff29cd8a6e0ecf6662224
Janet Chiem <illuistoffee@gmail.com>
2025-11-15 19:58:51 -0800

    login page will now flash incorrect password if it can't find username & password in database

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -3,6 +3,8 @@ from ..forms import LoginForm
 #from app import myapp_obj
 from flask import current_app as myapp_obj
 
+from ..models import Student
+
 
 #login page
 @myapp_obj.route('/auth/login', methods = ['GET', 'POST'])
@@ -12,11 +14,14 @@ def login():
     if request.method == 'POST':
         if form.validate_on_submit():
             #ADD LOGIC HERE (like add user to database, etc)
-
-            return redirect('/feature')
-        else:
-            flash('Not successful, data missing!')
-            return render_template('auth/login.html', form=form)
+            username= form.username.data
+            password = form.password.data
+            user=Student.query.filter_by(username=username).first()
+            if user and check_password_hash(user.password, password):
+                return redirect('/feature')
+            else:
+                flash('Not successful, data missing or incorrect!')
+                return redirect('/auth/login')
     #this render is the FIRST render for new users
     return render_template('auth/login.html', form = form)
 
