cdb5879e6b2045fd9d1a0b5dfeffe1eded187f3e
Bryan D. Morales Sosa <bryan.moralessosa@sjsu.edu>
2025-12-07 15:33:32 -0800

    Updated courses.html with a block that prints student's courses out in a table format. Provides a submit button that lets them DROP a class. Pressing DROP or ENROLL (if you're already enrolled) will now delete the data from enrollment (based on studentid, courseid)

--- app/main/routes.py
+++ app/main/routes.py
@@ -94,16 +94,27 @@ def showCourses():
             return render_template('main/courses.html', username=username, isStudent=isStudent, courses=courses, form=form)
         else: #student view - make form for students to enroll in courses instead (enroll button per course)
             enrollForm = CourseSignUp()
+            #also retrieve the enrolled courses by the student's id, then get those courses
+            student = Student.query.filter_by(username=session.get("username")).first()
+            #by JOIN-ing the Course relation with the Enrollment relation ON student_id = student.id, we get the student's courses
+            enrolledCourses = Course.query.join(Enrollment).filter(Enrollment.student_id == student.id)
+            studentCourses = enrolledCourses.all() #this is neccessary to turn into a list
 
             #logic: when the 'Enroll' button is pressed, we want to get the student's id and course's id to add to Enrollments
             if request.method == 'POST':
                 #the data for the course id is in the request form
                 course_id = request.form.get('course_id')
                 if course_id:
-                    student = Student.query.filter_by(username=session.get("username")).first()
                     already_enrolled = Enrollment.query.filter_by(student_id=student.id, course_id=course_id).first()
                     if already_enrolled:
-                        flash("You're already enrolled to this class!")
+                        course = Course.query.filter_by(id=course_id).first()
+                        if course:
+                            course.students_enrolled -= 1
+                        #delete/drop already_enrolled tuple from relation Enrollment
+                        db.session.delete(already_enrolled)
+                        db.session.commit()
+                        flash("Dropped the class!")
+                        return redirect('/courses')
                     else:
                         course = Course.query.filter_by(id=course_id).first()
                         newEnroll = Enrollment(student_id=student.id, course_id=course.id)
@@ -114,7 +125,7 @@ def showCourses():
                         flash('Succesfully enrolled!')
                     return redirect('/courses')
 
-            return render_template('main/courses.html', username=username, isStudent=isStudent, courses=courses, enrollForm = enrollForm)
+            return render_template('main/courses.html', username=username, isStudent=isStudent, courses=courses, enrollForm = enrollForm, studentCourses = studentCourses)
     #redirect non-users to log in first
     flash('You are not logged in!')
     return redirect('/auth/login')
