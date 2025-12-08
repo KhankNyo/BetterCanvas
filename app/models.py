7ba939086c4b063cfa4ee6276a3726688c0ddbd1
Khanh <ngokhanh809@gmail.com>
2025-12-07 21:36:24 -0800

    Fixed typo, ignored utility script to switch-merge-push

--- app/models.py
+++ app/models.py
@@ -4,7 +4,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 g_EMAIL_STRING_CAPACITY = 128
 g_NAME_STRING_CAPACITY = 64
 g_PASSWORD_STRING_CAPACITY = 64
-g_DESCRIPTION_STRING_CAPCITY = 1024
+g_DESCRIPTION_STRING_CAPACITY = 1024
 g_TIMESTAMP_STRING_CAPACITY = 64
 
 class Student(db.Model):
