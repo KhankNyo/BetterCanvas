01f26a94707b2b909c967f57f7d027db2d8e8e24
Janet Chiem <illuistoffee@gmail.com>
2025-12-08 21:24:34 -0800

    added 6 more tests + updated README

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
-# Set Up
+# M2 - Set Up
 Run the command "git clone https://github.com/KhankNyo/BetterCanvas" in your terminal
 then cd into BetterCanvas directory. 
-Run the file run.py using python3 run.py in your terminal. Put in the given link in your browser. 
-### IMPORTANT: Make sure you are logged out on our website! 
+Run the file run.py using python3 run.py in your terminal. Put in the link "http://127.0.0.1:5000/logout" in your browser. 
+### IMPORTANT: Make sure you are logged out on our website! Changes to the database were made.
 Then, to access different parts of the website, please log in or register an account. Teacher and student accounts see different things on the website. 
+
+If you do not wish to register, input "tester" into the username and "password123" for the password (without the quotes). This logs you into a premade teacher account. The rest of the accounts found in the /people tab also have the password "password123" as well. 
 
 > Here is a screenshot from an example runtime after logging in as a teacher:
 ![teacher's runtime view](images/runtimess.png "BetterCanvas")
@@ -16,12 +18,11 @@ Then, to access different parts of the website, please log in or register an acc
 From the BetterCanvas directory, cd to the test directory in the terminal using "cd app/tests" (without the quotes). 
 ### IMPORTANT: run each test separately in the terminal. 
 Ex. input (without the quotes) "pytest test_routes_home.py" and do the same for each test. Here are all the tests that should be run. Feel free to copy and paste:  
+- pytest test_forms.py
 - pytest test_routes_home.py 
 - pytest test_routes_login.py
 - pytest test_routes_redirect.py
-- pytest test_routes_error.py 
-- pytest test_models.py
-- pytest test_forms.py
+- pytest test_routes_error.py
 
 
 # Team Roles
--- app/tests/test_forms.py
+++ app/tests/test_forms.py
@@ -1,2 +1,56 @@
-import pytest
+import unittest, pytest
+from flask import Flask
+from app.forms import LoginForm, RegistrationForm, AnnouncementForm
 
+class TestLoginForm(unittest.TestCase): #test if user is able to login and if the login form is using the right information
+    def setUp(self):
+        self.app = Flask(__name__)
+        self.app.config['WTF_CSRF_ENABLED'] = False
+
+    def test_valid_login(self):
+        with self.app.test_request_context(method= 'POST', data = {'username': 'tester','password': 'password123', 'remember_me': 'y'}):
+            form = LoginForm()
+            self.assertTrue(form.validate_on_submit())
+            self.assertEqual(form.username.data, 'tester')
+            self.assertEqual(form.password.data, 'password123')
+            self.assertTrue(form.remember_me.data)
+    def test_invalid_login(self):
+        with self.app.test_request_context(method='POST', data = {'username': 'tester',}):
+            form = LoginForm()
+            self.assertFalse(form.validate_on_submit())
+
+class TestRegistrationForm(unittest.TestCase):
+    def setUp(self):
+        self.app = Flask(__name__)
+        self.app.config['WTF_CSRF_ENABLED'] = False
+
+    def test_valid_registration(self):
+        with self.app.test_request_context(method='POST', data = {'username': 'teacher','password': 'password123',
+                                                                  'email': 'teacher@email.com', 'isTeacher': True}):
+            form = RegistrationForm()
+            self.assertTrue(form.validate_on_submit())
+            self.assertEqual(form.username.data, 'teacher')
+            self.assertEqual(form.password.data, 'password123')
+            self.assertTrue(form.email.data)
+            self.assertTrue(form.isTeacher.data)
+
+    def test_invalid_registration(self):
+        with self.app.test_request_context(method='POST', data = {'password': 'password123',}):
+            form = RegistrationForm()
+            self.assertFalse(form.validate_on_submit())
+
+class TestAnnouncement(unittest.TestCase):
+    def setUp(self):
+        self.app = Flask(__name__)
+        self.app.config['WTF_CSRF_ENABLED'] = False
+
+    def test_valid_registration(self):
+        with self.app.test_request_context(method='POST', data = {'title': 'TITLE','description': 'This is a description'}):
+            form = AnnouncementForm()
+            self.assertTrue(form.validate_on_submit())
+            self.assertEqual(form.title.data, 'TITLE')
+            self.assertEqual(form.description.data, 'This is a description')
+    def test_invalid_registration(self):
+        with self.app.test_request_context(method='POST', data = {'title': 'TITLE',}):
+            form = AnnouncementForm()
+            self.assertFalse(form.validate_on_submit())
